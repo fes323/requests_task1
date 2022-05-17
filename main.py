36ee16a197b6357c082b622344ddf5a7473310fc
Maksim Legeyda <Maksim.Legeyda@yandex.ru>
2022-05-18 01:49:48 +0300

    add input

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import requests
 
-hulk_url = requests.get("https://superheroapi.com/api/2619421814940190/search/hulk")
-hulk_ID = hulk_url.json()['results'][0]['id']
+First_hero = input("Введите имя первого героя: ")
+Second_hero = input("Введите имя второго героя: ")
+Third_hero = input("Введите имя третьего героя: ")
+
+First_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{First_hero}")
+hulk_ID = First_hero.json()['results'][0]['id']
 hulk_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + hulk_ID)
 hulk_int = hulk_original_url.json()['powerstats']['intelligence']
 
-captain_url = requests.get("https://superheroapi.com/api/2619421814940190/search/captain")
-captain_ID = captain_url.json()['results'][0]['id']
+Second_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{Second_hero}")
+captain_ID = Second_hero.json()['results'][0]['id']
 captain_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + captain_ID)
 captain_int = captain_original_url.json()['powerstats']['intelligence']
 
-thanos_url = requests.get("https://superheroapi.com/api/2619421814940190/search/thanos")
-thanos_ID = thanos_url.json()['results'][0]['id']
+Third_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{Third_hero}")
+thanos_ID = Third_hero.json()['results'][0]['id']
 thanos_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + thanos_ID)
 thanos_int = thanos_original_url.json()['powerstats']['intelligence']
 
@@ -23,11 +27,11 @@ class SuperHero(object):
     def __repr__(self):
         return "%s(%r)" % (self.name, self.int)
 
-hulk = SuperHero('Hulk', hulk_int)
-captain = SuperHero('Captain Amerika', captain_int)
-thanos = SuperHero('Thanos', thanos_int)
+First_hero = SuperHero('Hulk', hulk_int)
+Second_hero = SuperHero('Captain Amerika', captain_int)
+Third_hero = SuperHero('Thanos', thanos_int)
 
-heroes = [hulk, captain, thanos]
+heroes = [First_hero, Second_hero, Third_hero]
 
 def intelligence(superhero):
     return superhero.int
