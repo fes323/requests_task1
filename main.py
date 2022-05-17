8ddfa82d1d72c829a7ecefd1a4c771e15f201e27
Maksim Legeyda <Maksim.Legeyda@yandex.ru>
2022-05-18 01:56:27 +0300

    fix variable names

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import requests
 
-First_hero = input("Введите имя первого героя: ")
-Second_hero = input("Введите имя второго героя: ")
-Third_hero = input("Введите имя третьего героя: ")
+first_hero = input("Введите имя первого героя: ")
+second_hero = input("Введите имя второго героя: ")
+third_hero = input("Введите имя третьего героя: ")
 
-First_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{First_hero}")
-hulk_ID = First_hero.json()['results'][0]['id']
-hulk_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + hulk_ID)
-hulk_int = hulk_original_url.json()['powerstats']['intelligence']
+first_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{first_hero}")
+first_hero_ID = first_hero.json()['results'][0]['id']
+first_hero_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + first_hero_ID)
+first_hero_int = first_hero_original_url.json()['powerstats']['intelligence']
 
-Second_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{Second_hero}")
-captain_ID = Second_hero.json()['results'][0]['id']
-captain_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + captain_ID)
-captain_int = captain_original_url.json()['powerstats']['intelligence']
+second_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{second_hero}")
+second_hero_ID = second_hero.json()['results'][0]['id']
+second_hero_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + second_hero_ID)
+second_hero_int = second_hero_original_url.json()['powerstats']['intelligence']
 
-Third_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{Third_hero}")
-thanos_ID = Third_hero.json()['results'][0]['id']
-thanos_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + thanos_ID)
-thanos_int = thanos_original_url.json()['powerstats']['intelligence']
+third_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{third_hero}")
+third_ID = third_hero.json()['results'][0]['id']
+third_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + third_ID)
+third_int = third_original_url.json()['powerstats']['intelligence']
 
 
 class SuperHero(object):
@@ -27,9 +27,9 @@ class SuperHero(object):
     def __repr__(self):
         return "%s(%r)" % (self.name, self.int)
 
-First_hero = SuperHero('Hulk', hulk_int)
-Second_hero = SuperHero('Captain Amerika', captain_int)
-Third_hero = SuperHero('Thanos', thanos_int)
+First_hero = SuperHero('Hulk', first_hero_int)
+Second_hero = SuperHero('Captain Amerika', second_hero_int)
+Third_hero = SuperHero('Thanos', third_int)
 
 heroes = [First_hero, Second_hero, Third_hero]
 
