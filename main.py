cb3f787eee82f1549f4e13f54c8f78c72bd5adcc
Maksim Legeyda <Maksim.Legeyda@yandex.ru>
2022-05-18 22:51:20 +0300

    rework

--- main.py
+++ main.py
@@ -1,40 +1,25 @@
 import requests
 
-first_hero = input("Введите имя первого героя: ")
-second_hero = input("Введите имя второго героя: ")
-third_hero = input("Введите имя третьего героя: ")
+heroes_list = ['Hulk', 'Captain', 'Thanos']
+res_id = []
+heroes_intel = []
 
-first_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{first_hero}")
-first_hero_ID = first_hero.json()['results'][0]['id']
-first_hero_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + first_hero_ID)
-first_hero_int = first_hero_original_url.json()['powerstats']['intelligence']
+for i in heroes_list:
+    res_url_tmp = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{i}")
+    res_id_tmp = res_url_tmp.json()['results'][0]['id']
+    res_id.append(res_id_tmp)
 
-second_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{second_hero}")
-second_hero_ID = second_hero.json()['results'][0]['id']
-second_hero_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + second_hero_ID)
-second_hero_int = second_hero_original_url.json()['powerstats']['intelligence']
+for i in res_id:
+    id_url = requests.get(f"https://superheroapi.com/api/2619421814940190/{i}")
+    heroes_int_tmp = id_url.json()['powerstats']['intelligence']
+    heroes_intel.append(heroes_int_tmp)
 
-third_hero = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{third_hero}")
-third_ID = third_hero.json()['results'][0]['id']
-third_original_url = requests.get("https://superheroapi.com/api/2619421814940190/" + third_ID)
-third_int = third_original_url.json()['powerstats']['intelligence']
+heroes_intel = list(map(int,heroes_intel))
 
+heroes_dict = {}
+for i in range(0, len(heroes_list)):
+    heroes_dict[heroes_list[i]] = heroes_intel[i]
 
-class SuperHero(object):
-    def __init__(self, name, int):
-        self.name = name
-        self.int = int
-    def __repr__(self):
-        return "%s(%r)" % (self.name, self.int)
+top_int =  max(zip(heroes_dict.values(),heroes_dict.keys()) )
 
-First_hero = SuperHero('Hulk', first_hero_int)
-Second_hero = SuperHero('Captain Amerika', second_hero_int)
-Third_hero = SuperHero('Thanos', third_int)
-
-heroes = [First_hero, Second_hero, Third_hero]
-
-def intelligence(superhero):
-    return superhero.int
-
-heroes_sorted = sorted(heroes, key = intelligence)
-print(heroes_sorted[0])
+print(top_int)
